e9ab55b01d49ff2765432a7dc4ba923d3ba2b925
Connor Hindley <connor.hindley@tanium.com>
2023-01-10 18:21:03 -0700

    Loudly fail if not enabled

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/connyay/bazel-goboring/boring"
 )
 
 func main() {
 	fmt.Printf("goboring enabled=%t\n", boring.Enabled())
+	if !boring.Enabled() {
+		os.Exit(1)
+	}
 }
