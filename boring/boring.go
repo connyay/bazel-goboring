601650c7f85632bea777d0a1f8515660d0fbbf41
Connor Hindley <connor.hindley@tanium.com>
2023-01-10 18:26:40 -0700

    Add init() logging

--- boring/boring.go
+++ boring/boring.go
@@ -3,8 +3,14 @@
 package boring
 
 import (
+	"log"
+
 	"crypto/boring"
 )
+
+func init() {
+	log.Printf("----- in boring.go -----")
+}
 
 // Enabled reports whether BoringCrypto handles supported crypto operations.
 func Enabled() bool {
--- boring/notboring.go
+++ boring/notboring.go
@@ -4,6 +4,12 @@
 // IDE friendly.
 package boring
 
+import "log"
+
+func init() {
+	log.Printf("----- in boring.go -----")
+}
+
 // Enabled reports whether BoringCrypto handles supported crypto operations.
 func Enabled() bool {
 	return false
